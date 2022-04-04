7eee209ac51f0759e75d25396cce86280227e425
SmallPP420 <SmallPP420@small.com>
2022-04-04 11:46:18 +0100

    mtk bootctrl
    Change-Id: Ie29541c394a6c0456d7f37d381d1be9ccd3aa2a6

--- device.mk
+++ device.mk
@@ -2,3 +2,47 @@ LOCAL_PATH := device/nokia/WSP_sprout
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root) \
         $(LOCAL_PATH)/prebuilt/dtb.img:dtb.img
+
+# A/B
+AB_OTA_PARTITIONS += \
+    boot \
+    custom \
+    dtbo \
+    elabel \
+    lk \
+    md1img \
+    preloader \
+    scp \
+    spmfw \
+    sspm \
+    system \
+    tee \
+    vbmeta \
+    vendor \
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.1-impl \
+    android.hardware.boot@1.1-service
+
+PRODUCT_PACKAGES += \
+    bootctrl.mt6761
+
+PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+    bootctrl.mt6761 \
+    libgptutils \
+    libz \
+    libcutils
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_verifier \
+    update_engine_sideload
